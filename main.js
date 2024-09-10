2b14cd6cbf1cb8c74a76d8e2fe9e89ec4734b435
Starist <codemitstarist@gmail.com>
2024-09-10 22:01:30 +0100

    showint required and unvalid messages

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+let submit = document.querySelector("button[type='submit']");
+let emptyGroup = document.querySelectorAll('span.empty');
+
+submit.addEventListener("click", (ele) => {
+        ele.preventDefault();
+
+        if(textFirst.value.length == 0) {
+                emptyGroup[0].style.diplay = 'block';
+        }
+        if(textLast.value.length == 0) {
+                emptyGroup[1].style.diplay = 'block';
+        }
+
+})
+
+let textFirst = document.querySelector(['input[type="text']);
+let textLast = document.querySelectorAll(['input[type="text'])[1];
+let emailInput = document.querySelector(['input[type="email']);
+let aue = document.querySelector(['input[type="email']);
+let queryGeneral = document.querySelector(['input[type="radio']);
+let queryRequest = document.querySelectorAll(['input[type="radio'])[1];
+let textArea = document.querySelector('textarea');
+let checkboxInput = document.querySelector(['input[type="checkbox']);
 let checkInput = document.querySelector('.check input');
 let greenBack = document.querySelector('.check .greenBack');
-
-checkInput.addEventListener('click', (ele) => {
-        greenBack.style.display = 'block';
-
-});
-greenBack.addEventListener('click', (ele) => {
-        greenBack.style.display = 'none';
-});
